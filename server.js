8d3a0c866df348539ba2fb009ca53a4b4dbaf22e
jpgotswagg644 <jpgotswagg644@gmail.com>
2025-03-14 13:58:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "vaniakostadinova15513@gmail.com";
-//const pass = "xmfloskqejlzktyb";
-// 24 jan
+const pass = "xmfloskqejlzktyb";
+// 14 apirl 
+// i paid
 // Middleware
 app.use(express.json());
 
